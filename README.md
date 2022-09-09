c3da87a59c05666256e7ab2cc2b71db62d15027c
fkavache <vkats15@freeuni.edu.ge>
2022-09-09 21:27:09 +0400

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FHFIF
 Foster's Home for Imaginary Friends
+
+Having fun with:
+* C++, CMake
+* Boost/Beast, Boost/Asio, RESTful
+* nlohmann JSON
+* JWT
+* SQLAPI++/PostgreSQL
+* **Bloo**, **Mac**, Mr. Herriman, Martha Foster, et al.
+
+![FHFIF](https://static.wikia.nocookie.net/fhif/images/d/da/Cheese0.png "FHFIF")
+
+* and Cheese.
--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,11 @@ using namespace std;
 #define PORT       8080
 
 int main() {
-//    DAL::GetInstance()->connect(DB, USER, PASSWORD, SA_PostgreSQL_Client);
-//
-//    APIServer::run(LOCALHOST, PORT);
-//
-//    DAL::GetInstance()->disconnect();
+    DAL::GetInstance()->connect(DB, USER, PASSWORD, SA_PostgreSQL_Client);
+
+    APIServer::run(LOCALHOST, PORT);
+
+    DAL::GetInstance()->disconnect();
 
     cout << "\n============== FHFIF ==============\n" << endl;
     return 0;
